580c6544131f1b8ae37d6a97a2b8dc6e3acc587c
Ganesh123AS <91936810+Ganesh123AS@users.noreply.github.com>
2022-05-06 11:46:31 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("#login-open-btn").addEventListener("click",function(){
+document.querySelector(".login-form").style.display = "block";
+})
+
+document.querySelector("#login-close-btn").addEventListener("click",function()
+{
+  document.querySelector(".login-form").style.display = "none";
+})
---
+++ js/search.js
@@ -0,0 +1,8 @@
+
+document.querySelector("#search-open-btn").addEventListener("click",function(){
+    document.querySelector(".search-form").style.display = "block";
+  })
+  document.querySelector("#search-close-btn").addEventListener("click",function()
+  {
+    document.querySelector(".search-form").style.display = "none";
+  })
